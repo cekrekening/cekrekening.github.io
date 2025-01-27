c7311d42c864fc33155855c65c4d6107713bdb6f
Vava Heirro <vavaheirro@live.com>
2025-01-28 04:41:34 +0700

    feat(app): add trakteer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./layout/Footer";
 import Cekrek from "./Cekrek";
+import Trakteer from "./layout/Trakteer";
 
 export default function App() {
   return (
@@ -8,7 +9,8 @@ export default function App() {
       <div className="container-flex flex flex-col justify-center h-screen overflow-hidden">
         <main className="flex-1 overflow-hidden 2xl:px-20 xl:px-20 lg:px-20 md:px-20 px-10 py-16">
           <div className="h-screen">
-          <Cekrek />
+            <Cekrek />
+            <Trakteer />
           </div>
         </main>
         <Footer />
