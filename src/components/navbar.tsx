49ef1448753b35cda17a3bff02b1f3ccfe3788c4
Vava Heirro <vavaheirro@live.com>
2025-02-23 01:36:54 +0700

    fix(navbar): update title and add mobile docs link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,13 +11,13 @@ import { HeartFilledIcon, GithubIcon } from "@/components/icons";
 
 export const Navbar = () => {
   return (
-    <HeroUINavbar maxWidth="md" position="sticky">
+    <HeroUINavbar maxWidth="lg" position="sticky">
       <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-4">
-          <Link isExternal href={siteConfig.links.docs} title="Twitter">
+          <Link isExternal href={siteConfig.links.docs} title="Documentation">
             <HeartFilledIcon className="text-default-500" />
           </Link>
           <Link isExternal href={siteConfig.links.github} title="GitHub">
@@ -28,6 +28,9 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <Link isExternal href={siteConfig.links.docs} title="Documentation">
+            <HeartFilledIcon className="text-default-500" />
+          </Link>
         <Link isExternal href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
